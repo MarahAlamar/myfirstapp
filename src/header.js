58cbe334bf038f6f58f5a1a99e34c62ec535a3f9
MarahAlasmar <alasmarmarah567@gmail.com>
2023-09-28 06:07:14 +0300

    initil commit

--- src/header.js
+++ src/header.js
@@ -13,7 +13,7 @@ function header(){
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Food Recipes</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
