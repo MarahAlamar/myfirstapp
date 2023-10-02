af038f0d70221130db04fd26a175e67f4b52328e
MarahAlasmar <alasmarmarah567@gmail.com>
2023-10-02 05:25:02 +0300

    update

--- src/HealthyFood.js
+++ src/HealthyFood.js
@@ -1,8 +1,44 @@
+import data from './data.json';
+import CardComp from './card';
+import './main.css';
+import  { useState } from 'react';
+
+
 function HealthyFood(){
+    let [selectCategory, setSelectCategory] = useState('All');
+
+    function chageCategory(event){
+        setSelectCategory(event.target.value);
+    }
+
+    let filteredFood = data;
+    if (selectCategory !== 'All') {
+        filteredFood = data.filter(function (item) {
+            return item.category === selectCategory;
+        });
+    }
+
     return(
+
         <>
-        Hello world
-        </>
+           <select id="select-category" onChange={chageCategory} value={selectCategory}>
+                <option value="All">All</option>
+                <option value="Main Course">Main Course</option>
+                <option value="Desserts">Dessert</option>
+                <option value="Salads">Salads</option>
+                <option value="Appetizers">Appetizers</option>
+            </select>
+        
+            <div id="contantmain">
+                {filteredFood.map(function (item) {
+                    return (
+<CardComp image_url={item.image_url} title={item.title} description={item.description} />
+                    )
+                }) // for function
+                }
+                            </div>
+
+   </>
     )
 }
 
